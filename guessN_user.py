65eb8572c81c1106539a693a1fbfc04263140d79
jdmorales89 <jdmorales89@gmail.com>
2021-11-19 15:04:32 +0100

    added guessN_user.py

---
+++ guessN_user.py
@@ -0,0 +1,19 @@
+import random
+
+def guess(x):
+    """It asks to user to guess the a number between 0 and x.
+
+    Args:
+        x (int): Top number limit for guessing
+    """
+    random_number = random.randint(0, x)
+    guess = -1
+    while guess != random_number:
+      guess = int(input(f'Guess a number between 0 and {x}: '))
+      if guess < random_number:
+          print('\nSorry, quess again. Too low.')
+      elif guess > random_number:
+          print('\nSorry, quess again. Too high.')
+    print(f'\nYay, congrats. You have guessed the random number {random_number}.')
+      
+guess(10)
